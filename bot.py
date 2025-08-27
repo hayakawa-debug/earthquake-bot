e6432c767e616de3d687877cee2878cfa5d2781c
hayakawa-debug <hayakawa5127@gmail.com>
2025-08-27 20:28:48 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ import tweepy
 import os
 
 # X APIキー（GitHub ActionsのSecretsから読み込む）
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+API_KEY = os.getenv("BhyXOHjAKtDprBFDbSJFVxqxd")
+API_SECRET = os.getenv("BUIEYkVKjtemb7DIItqdqPvGuvkR6ziGqEZqhfAEebAj30uJpf
+")
+ACCESS_TOKEN = os.getenv("1658403711071457280-eJ5BI6EDDefwEQLNLb9woeyINqvb37")
+ACCESS_SECRET = os.getenv("8rgJJu0wBLaBp1EEXstmiT5Stu7zL61JMAiNZXttiuTT6")
 
 auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -23,3 +24,4 @@ link = entry.link
 # ツイート内容
 tweet = f"【気象庁 地震情報】\n{title}\n{link}"
 api.update_status(tweet)
+
